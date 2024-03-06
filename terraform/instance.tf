5ae544c39f4c922e78ff5d7c81ab50193359cdc2
bipul prasai <bipulprasai2020@gmail.com>
2024-03-07 03:04:24 +1100

    changes made in resources

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -1,41 +1,7 @@
-# aws key_pair
-
-resource "aws_key_pair" "key-tf" {
-  key_name   = "key-tf"
-  public_key = file("${path.module}/id_rsa.pub")
-}
-
-#  aws_security_group
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-
-  dynamic "ingress" {
-    for_each = [22, 80, 443, 3306, 27017]
-    iterator = port
-    content {
-      description = "TLS from VPC"
-      from_port   = port.value
-      to_port     = port.value
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  }
-
-  egress {
-from_port = 0
-to_port = 0
-protocol = "-1"
-cidr_blocks = ["0.0.0.0/0"]
-ipv6_cidr_blocks = ["::/0"]
-  }
-
-}
-
 # aws EC2_instance
 resource "aws_instance" "web" {
-  ami                    = "ami-0d6f74b9139d26bf1"
-  instance_type          = "t2.micro"
+  ami                    = var.image_id
+  instance_type          = var.instance_type
   key_name               = aws_key_pair.key-tf.key_name
   vpc_security_group_ids = ["${aws_security_group.allow_tls.id}"]
   tags = {
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,5 @@
+ports         = [22, 80, 443, 3306, 27017]
+image_id      = "ami-0d6f74b9139d26bf1"
+instance_type = "t2.micro"
+access_key    = "AKIAUG6WO2DICOYWPGFZ"
+secret_key    = "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"
