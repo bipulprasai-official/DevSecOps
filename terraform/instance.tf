10c675edbd5a48a4d7b840fa326cf26d2ce8bad0
bipul prasai <bipulprasai2020@gmail.com>
2024-03-07 02:18:59 +1100

    added resources in tf file

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -1,16 +1,35 @@
-resource "aws_key_pair" "key-tf" {
-  key_name   = "key-tf"
-  public_key = file("${path.module}/id_rsa.pub")
-}
+# # aws key_pair
 
-output "keyname" {
-  value = "${aws_key_pair.key-tf.key_name}"
-}
+# resource "aws_key_pair" "key-tf" {
+#   key_name   = "key-tf"
+#   public_key = file("${path.module}/id_rsa.pub")
+# }
 
+# # aws EC2_instance
 # resource "aws_instance" "web" {
 #   ami           = "ami-0d6f74b9139d26bf1"
 #   instance_type = "t2.micro"
+#   key_name = "${aws_key_pair.key-tf.key_name}"
 #   tags = {
 #     Name = "terraform-learn-state-ec2"
 #   }
 # }
+
+#  aws_security_group
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+
+  dynamic "ingress" {
+    for_each = [22, 80, 443, 3306, 27017]
+    iterator = port
+    content {
+      description = "TLS from VPC"
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+}
